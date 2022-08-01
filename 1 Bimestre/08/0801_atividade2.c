c60e2e7cec760773266b1e256782243c0947bd03
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-08-01 20:00:31 -0300

    atividade hipotenusa

---
+++ 1 Bimestre/08/0801_atividade2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    float catA, catO, hip;
+    hip = 0; // limpar variavel de calculo
+
+    printf("Digite o valor do cateto oposto: ");
+    scanf("%f", &catO);
+    printf("Digite o valor do cateto adjacente: ");
+    scanf("%f", &catA);
+
+    // a funcao pow() é usada para exponenciacao e
+    // recebe dois parametros
+    // 1º: variavel ou valor da base
+    // 2º: valor do expoente
+
+    hip = sqrt(pow(catA, 2) + pow(catO, 2));
+    printf("\nA hipotenusa: %0.2f", hip);
+    getch();
+}
