c9583f7e172401fe1538c2bf7a750493b05e9cd0
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-08-01 21:33:59 -0300

    laco de repeticao dado

--- 1 Bimestre/08/0801_atividade6_laco.c
+++ 1 Bimestre/08/0801_atividade6_laco.c
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 
 int main() {
+
+    // 9. Escreva um algoritmo que imprima todas as possibilidades
+    // de que no lançamento de dois dados tenhamos o valor de 7 como
+    // resultado da soma dos valores de cada lado.
+
     int i, j;
 
     for(i = 1; i <= 6; i++){
