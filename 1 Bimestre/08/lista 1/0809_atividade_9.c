b7f9a0d9d1031df4057800e580d6187f3be326e3
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-08-09 17:09:28 -0300

    aitivdade 9 nao finalizada

---
+++ 1 Bimestre/08/lista 1/0809_atividade_9.c
@@ -0,0 +1,40 @@
+/* 9) Fazer um algoritmo que leia a média bimestral de 5 alunos e mostre a nota mais
+alta, a nota mais baixa e média da turma. (DO WHILE). */
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define length 5
+
+int main(){
+    float notas[length], maiorNota = 0, menorNota = 0, media = 0;
+    int i = 0;
+
+    do{
+        printf("Digita a nota do aluno %i...: ", i);
+        scanf("%f", &notas[i]);
+
+        if(notas[i] > maiorNota){
+            maiorNota = notas[i];
+        }
+
+        if(notas[i] < ){
+            menorNota = notas[i];
+        }
+        printf("-------------------\n");
+        printf("O aluno %i tem nota %.2f \n", i, notas[i]);
+        printf("-------------------\n");
+
+        media += notas[i];
+        i++;
+    } while (i < length);
+
+    media = media / length;
+
+    printf("Maior nota...: %.2f \n", maiorNota);
+    printf("Menor nota...: %.2f \n", menorNota);
+    printf("Media da turma...: %.2f \n", media);
+    
+}
