be43eab1552d873cca8c25e2dcf4a661582c588b
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-08-10 00:41:43 -0300

    atividade 10 nao finalizada

---
+++ 1 Bimestre/08/lista 1/0810_atividade_10.c
@@ -0,0 +1,55 @@
+/* 10) Em uma cidade havia um total de 5 eleitores (todos foram votar). Havia três opções
+de candidato, o candidato número 11, o número 22 e o número 33. Além desses
+candidatos, também havia a possibilidade de votar em branco que correspondia ao
+número 0 (zero), ou o voto nulo que correspondia a qualquer outro número diferente
+de 0, 11, 22 e 33. 
+
+O objetivo do algoritmo é ler o voto de todos os eleitores da cidade,
+e calcular qual o total e a porcentagem de votos de cada um dos candidatos. O
+programa também deve calcular o total e a porcentagem de votos brancos ou nulos.
+No final o exercício deve imprimir todos esses cálculos. (DO WHILE). */
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+#define length 5
+
+int main() {
+    int votos[length], i = 0, opcao = 0;
+    do{
+        printf("-----Informe seu canditado----\n");
+        printf("[11] Candidato 1\n");
+        printf("[22] Candidato 2\n");
+        printf("[33] Candidato 3\n");
+        printf("[0] Em Branco\n");
+        printf("[Outros] Votar Nulo\n");
+        printf("Escolha...: ");
+        scanf("%i", &opcao);
+
+        switch(opcao){
+            case 11:
+                votos[0]++;
+                break;
+
+            case 22:
+                votos[1]++;
+                break;
+            
+            case 33:
+                votos[2]++;
+                break;
+            
+            case 0:
+                votos[3]++;
+                break;
+            
+            default:
+                votos[4]++;
+                break;
+        }
+
+        i++;
+
+    } while(i < length);
+}
